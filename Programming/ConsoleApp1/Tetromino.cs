153dbbcafc16ca753b570e18e9e2f0940cd0ddc9
SgnX <sahsapro777@gmail.com>
2025-11-06 22:03:20 +0400

    тестовый коммит

--- Programming/ConsoleApp1/Tetromino.cs
+++ Programming/ConsoleApp1/Tetromino.cs
@@ -12,6 +12,15 @@ class Tetromino
             new int[,] {{1,1,0},{0,1,1}},
             new int[,] {{0,1,1},{1,1,0}},
         };
+public List<(int, int)> GetBlocks(int dx = 0, int dy = 0)
+        {
+            var blocks = new List<(int, int)>();
+            for (int i = 0; i < shape.GetLength(0); i++)
+                for (int j = 0; j < shape.GetLength(1); j++)
+                    if (shape[i, j] == 1)
+                        blocks.Add((x + j + dx, y + i + dy));
+            return blocks;
+        }
      public static Tetromino GetRandom(Random rnd)
         {
             return new Tetromino(shapes[rnd.Next(shapes.Count)]);
